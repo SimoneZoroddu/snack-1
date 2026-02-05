224809ec7ce654e6497098d6bcb7beddb68ce2d5
SimoneZoroddu <lenviur@gmail.com>
2026-02-05 10:42:55 +0100

    con parita

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const number_2 = prompt('choice another number')
 
 if (number_1 > number_2) {
     alert(number_1)
-} else {
+} else if (number_1 < number_2){
         alert(number_2)
+    } else {
+        alert('avete fatto pari')
     }
-
-
