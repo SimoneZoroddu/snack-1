ddd247b2c545e23baa182dc14d888a2e55d03f02
SimoneZoroddu <lenviur@gmail.com>
2026-02-05 10:31:59 +0100

    end of exercise

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-A
+const number_1 = prompt('choice one number')
+const number_2 = prompt('choice another number')
+
+
+if (number_1 > number_2) {
+    alert(number_1)
+} else {
+        alert(number_2)
+    }
+
+
