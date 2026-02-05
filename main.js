587c2685910bc866b23a91a9f49407b5ae3386c6
SimoneZoroddu <lenviur@gmail.com>
2026-02-05 11:00:29 +0100

    correzione (aggiunta Number)

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const number_1 = prompt('choice one number')
-const number_2 = prompt('choice another number')
+const number_1 = Number(prompt('choice one number'))
+const number_2 = Number(prompt('choice another number'))
 
 
 if (number_1 > number_2) {
